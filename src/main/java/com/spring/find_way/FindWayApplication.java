3564ca69ba24bd69822362d048f5e27a73858105
Lim <dlawjdals0218@gmail.com>
2023-01-05 12:07:07 +0900

    프로젝트 시작

---
+++ src/main/java/com/spring/find_way/FindWayApplication.java
@@ -0,0 +1,13 @@
+package com.spring.find_way;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindWayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindWayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/find_way/FindWayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.find_way;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindWayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
